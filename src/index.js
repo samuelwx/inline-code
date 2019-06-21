05ee440ef17926e67bd90f1da0e93f656d45f06f
Samuel Wang <39785566+samuelwx@users.noreply.github.com>
2019-06-21 19:07:07 +0800

    Moved selection checks to inlineToolbar

--- src/index.js
+++ src/index.js
@@ -155,15 +155,11 @@ class InlineCode {
   /**
    * Check and change Term's state for current selection
    */
-  checkState(selection) {
+  checkState(selection, hasMath) {
     const termTag = this.api.selection.findParentTag(this.tag, InlineCode.CSS);
-    this.button.classList.toggle(this.iconClasses.active, !!termTag);
     
-    const parentBlock = this.api.selection.getParentBlock(selection);
-    if (!!parentBlock) {
-      const mathNodes = parentBlock.querySelectorAll('span.inline-math');
-      this.button.disabled = !!this.api.selection.hasOverlap(selection, mathNodes);
-    }
+    this.button.classList.toggle(this.iconClasses.active, !!termTag);
+    this.button.disabled = hasMath;
   }
 
   /**
